7157e2adf11a2ff81936f1cf0355a60f71af3399
ChungTran <anhchungite@gmail.com>
2018-05-30 14:58:47 +0700

    initial commit

---
+++ src/EnumUtils.php
@@ -0,0 +1,10 @@
+<?php
+namespace anhchungite\enum;
+
+class EnumUtils {
+    public static function week() {
+        return [
+            'Monday', 'Tuesday', 'Wenesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+        ];
+    }
+}
